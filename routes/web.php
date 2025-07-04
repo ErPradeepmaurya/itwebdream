d21f67aea0910d8c54e109af1ffe2099bb6bfffb
ErPradeepmaurya <pusmaurya3@gmail.com>
2025-07-05 02:10:13 +0530

    chnage

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use App\Http\Controllers\Frontend\FreeCommaController;
 
 Route::get('/', [MainController::class, 'index'])->name('welcome');
 Route::get('free-comma-separator-tool', [FreeCommaController::class, 'FreeCommaController'])->name('FreeComma.freecommatool');
+
+Route::get('/check-db', function () {
+    dd(env('DB_HOST'), env('DB_DATABASE'), env('DB_USERNAME'), env('DB_PASSWORD'));
+});
