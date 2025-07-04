aa7a627fe9408bfdb17f0f75e9f24b77227877b5
ErPradeepmaurya <pusmaurya3@gmail.com>
2025-07-05 02:07:50 +0530

    chnage

--- resources/views/FreeComma/freecommatool.blade.php
+++ resources/views/FreeComma/freecommatool.blade.php
@@ -49,7 +49,7 @@
         <p>Do you often need to take a spreadsheet of data and convert to a comma-delimited list? </p>
         <p>Be it for taking a list of zip codes or names to make an SQL query, or to take data from a CSV and be able to
             paste into an array.
-            At <a href="http://delim.co">delim.co</a> we make that just a little easier.</p>
+            At <a href="https://itwebdream.com">It Web DREAM</a> we make that just a little easier.</p>
         <p>Enter your non-delimited data on the left, hit the button, and boom, separated data on the right.
             Special configs are below if the defaults aren't what you need!</p>
     </section>
