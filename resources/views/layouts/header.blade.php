f915e31b71fcd0c8c0a37d9d42acda17e7d75262
ErPradeepmaurya <pusmaurya3@gmail.com>
2025-07-08 02:08:35 +0530

    changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,9 +10,7 @@
         content="A custom software development company specializing in educational software, e-commerce software, IT solutions, software development, and custom web applications.">
     <meta name="description"
         content="We specialize in web development, custom software development, educational platforms, and eCommerce software. Scalable, secure, and tailored to your business needs.">
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('asset/main/css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('asset/freecomma/maincomma.css') }}">
 
