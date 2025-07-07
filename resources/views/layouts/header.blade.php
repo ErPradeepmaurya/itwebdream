696219fd408e3860922191cc6464e9b7fa4e70ba
ErPradeepmaurya <pusmaurya3@gmail.com>
2025-07-08 02:06:48 +0530

    change

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 
 <head>
-    <title>@yield('title', 'Software Development and Freelance Services Provider')</title>
+    <title>@yield('title', 'Custom Web & Software Development')</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="{{ asset('asset/main/image/favicon1.png') }}">
     <meta name="keywords"
-        content="Service provider of collegiate software solutions, university-level software, Enterprise Resource Planning development, electronic commerce framework, property management software, and independent contractor services">
+        content="A custom software development company specializing in educational software, e-commerce software, IT solutions, software development, and custom web applications.">
     <meta name="description"
-        content="Providing software consulting and engineering services to educational institutions, enterprises, real estate, eCommerce, universities, and hospitals.">
+        content="We specialize in web development, custom software development, educational platforms, and eCommerce software. Scalable, secure, and tailored to your business needs.">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
