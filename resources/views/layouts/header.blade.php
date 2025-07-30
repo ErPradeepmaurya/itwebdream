c76d61c358524003e4644642b711bbd3121179e3
ErPradeepmaurya <pusmaurya3@gmail.com>
2025-07-31 02:58:52 +0530

    chnages in header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,10 @@
         content="We specialize in web development, custom software, eCommerce, and educational platforms—secure, scalable, and tailored to your business needs.">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('asset/main/css/main.css') }}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </head>
 
